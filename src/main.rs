80c52fbeff6ef6f6ad2092401d0fb58e7bf06962
rojer <rojermagnuson98@gmail.com>
2022-11-15 10:36:01 +0300

    Get file and convert to txt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::{io::ErrorKind, path::{Path, PathBuf}, process::Command};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -6,12 +7,33 @@ struct Args {
     /// Name of the person to greet
     #[arg(short, long)]
     file_name: String,
-
-    /// Number of times to greet
-    #[arg(short, long)]
-    command: String,
 }
 
 fn main() {
     let args = Args::parse();
+    let file_name = args.file_name;
+    let ext = Path::new(&file_name).extension().unwrap().to_str().unwrap();
+    match ext {
+        "pdf" => (),
+        _ => {
+            println!("Wrong extension");
+            return;
+        }
+    };
+
+    let output = match Command::new("pdftotext")
+        .args(["-layout", &file_name])
+        .output()
+    {
+        Ok(d) => (),
+        Err(e) => {
+            println!("Error while command was executing: {e}");
+            return;
+        }
+    };
+
+    let mut new_file = PathBuf::from(file_name);
+    new_file.set_extension("txt");
+    let new_file = new_file.to_str().unwrap().to_string();
+    println!("{new_file}");
 }
